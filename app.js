8c07ebeaf68fa6ab045d02b75b028325f9648a11
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-04-05 21:04:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// for(var i=0; i<=10; i++){
+//     if(i === 5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// for(var i=0; i<=10; i++){
+//     if(i === 5){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// var cities = ["Islamaabd","Karachi","Lahore","Multan"]
+// for(var i=0; i<cities.length; i++){
+//     // console.log(cities[i])
+//     if(cities[i] === "Lahore")
+//     continue;
+//     console.log(cities[i])
+// }
+
+// for(var i=1; i<=3; i++){
+//     document.write("Outer loop","<br>")
+//     for(var j=1; j<=5; j++){
+//         document.write("Inner Loop","<br>")
+//     }
+// }
+
+// var fname = ["Muzammil","Ali","Irfan"]
+// var lname= ["Saad","aman"]
+
+// for(var i = 0; i<fname.length; i++){
+//     for(var j=0; j<lname.length; j++){
+//         document.write(fname[i]," ",lname[j],"<br>")
+//     }
+//     document.write("<br>")
+// }
+
+
+for(var i = 5; i>=1; i--){
+    for(var j=1; j<=i; j++){
+        document.write(i)
+    }
+    document.write("<br>")
+}
+
+// for(var i=1; i<=5; i++){
+//     for(var j=1; j<=i; j++){
+//         document.write(i)
+//     }
+//     document.write("<br>")
+// }
